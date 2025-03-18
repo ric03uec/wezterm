d2c4c7357f0616eb7fc4bcf5e2dc092a5485aa0a
devashish <devashish@esper.io>
2025-03-18 16:45:21 -0700

    updating settings

--- wezterm.lua
+++ wezterm.lua
@@ -14,4 +14,19 @@ config.color_scheme = 'AdventureTime'
 -- and finally, return the configuration to wezterm
 config.color_scheme = 'Solarized (dark) (terminal.sexy)'
 
+config.adjust_window_size_when_changing_font_size = false
+-- font_size = 12.0, -- Adjust as needed
+
+
+--
+config.line_height = 1.0
+config.cell_width = 1.0
+
+config.window_padding = {
+  left = 0,
+  right = 0,
+  top = 0,
+  bottom = 0,
+}
+
 return config
