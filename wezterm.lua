ce519c7687f4223839d2cc3347b94362a4253d32
devashish <devashish@esper.io>
2025-05-02 15:47:54 -0700

    updating config

--- wezterm.lua
+++ wezterm.lua
@@ -1,32 +1,47 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
+local act = wezterm.action
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices
 
-config.font = wezterm.font 'Fira Code'
+config.font = wezterm.font("Fira Code")
 
 -- For example, changing the color scheme:
-config.color_scheme = 'AdventureTime'
+config.color_scheme = "AdventureTime"
 
 -- and finally, return the configuration to wezterm
-config.color_scheme = 'Solarized (dark) (terminal.sexy)'
+config.color_scheme = "Solarized (dark) (terminal.sexy)"
 
 config.adjust_window_size_when_changing_font_size = false
 -- font_size = 12.0, -- Adjust as needed
-
 
 --
 config.line_height = 1.0
 config.cell_width = 1.0
 
 config.window_padding = {
-  left = 0,
-  right = 0,
-  top = 0,
-  bottom = 0,
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
+}
+
+config.keys = {
+	-- Alt + LeftArrow => Ctrl+A (start of line)
+	{
+		key = "LeftArrow",
+		mods = "ALT",
+		action = wezterm.action.SendString("\x01"),
+	},
+	-- Alt + RightArrow => Ctrl+E (end of line)
+	{
+		key = "RightArrow",
+		mods = "ALT",
+		action = wezterm.action.SendString("\x05"),
+	},
 }
 
 return config
